53ee00b840cf8e2cccee2ad6e281ab3ec2a0e008
Michael Krebs <michikrbs@gmail.com>
2026-03-19 01:02:10 +0100

    chore: update room creation flow

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -12,7 +12,8 @@ export const rooms = sqliteTable('rooms', {
 	id: text('id')
 		.primaryKey()
 		.$defaultFn(() => crypto.randomUUID()),
-	title: text('title').notNull()
+	title: text('title').notNull(),
+	immichAlbumId: text('immich_album_id').notNull()
 });
 
 export const roomSchema = createInsertSchema(rooms);
