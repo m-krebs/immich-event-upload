984485a6d9449fa63c693d35fb963c5b2cb5b2f9
Michael Krebs <michikrbs@gmail.com>
2026-03-19 01:02:10 +0100

    chore: update room creation flow

---
+++ src/routes/admin/room/new/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Button } from '$lib/components/ui/button/index';
+	import { Input } from '$lib/components/ui/input/index';
+	import { createRoom } from './data.remote';
+</script>
+
+<h1 class="text-xl">Create new room</h1>
+
+<form {...createRoom}>
+	{#each createRoom.fields.title.issues() as issue}
+		<p class="issue">{issue.message}</p>
+	{/each}
+	<Input type="text" placeholder="Title" {...createRoom.fields.title.as('text')} />
+	<Button type="submit">Submit</Button>
+</form>
--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,17 @@ import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: {
+		experimental: {
+			remoteFunctions: true
+		},
+		adapter: adapter()
+	},
+	compilerOptions: {
+		experimental: {
+			async: true
+		}
+	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
 			filename.includes('node_modules') ? undefined : { runes: true }
